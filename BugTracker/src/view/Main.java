1afbadff8fb7b84728b27097d53c1ef9f9e0ff7a
Lucas Montoya <lucasmontoya15@hotmail.com>
2024-11-21 14:43:28 -0300

    se crea menu

--- BugTracker/src/view/Main.java
+++ BugTracker/src/view/Main.java
@@ -8,18 +8,33 @@ import model.Bug;
 // Clase Principal
 public class Main {
     public static void main(String[] args) {
-        // crear algunos bugs
-        BugTracker.crearBug();
-        // mostrar cantidad de bugs
-        JOptionPane.showMessageDialog(null, "Cantidad de bugs: " + Bug.getCantidadBug());
-        // mostrar datos de los bugs
-        /*
-         * for (int i = 0; i < BugTracker.cantidadBugs; i++) {
-         * JOptionPane.showMessageDialog(null, BugTracker.bugs[i].GetDetalle());
-         * }
-         */
-        // listar bugs
-        BugTracker.listarBugs();
 
+        boolean continuar = true;
+
+        while (continuar) {
+            String[] opciones = {"Crear Bug", "Listar Bug", "Modificar Bug", "Eliminar Bug", "Buscar Bug", "Informes", "Salir"};
+            int selection = JOptionPane.showOptionDialog(null, "¿Qué desea hacer?",
+                    "BugTracker", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones,
+                    opciones[0]);
+
+            switch (selection) {
+                case 0:
+                    // crear algunos bugs
+                    BugTracker.crearBug();
+                    break;
+                case 1:
+                    BugTracker.listarBugs();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+
+            }
+        }
     }
 }
