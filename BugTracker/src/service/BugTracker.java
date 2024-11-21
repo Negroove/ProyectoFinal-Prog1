c6470a44c94d85f5c16e70187734c952d32b5858
David Niz <dalos58.arg@gmail.com>
2024-11-21 19:27:26 -0300

    Busqueda por ID completa + menú tipos búsqueda

--- BugTracker/src/service/BugTracker.java
+++ BugTracker/src/service/BugTracker.java
@@ -3,6 +3,7 @@ package service;
 import javax.swing.JOptionPane;
 
 import model.Bug;
+import utils.Herramientas;
 
 // Logica / Reglas de Negocio
 
@@ -25,12 +26,75 @@ public class BugTracker {
     // listar bugs mostrando una lista general con los datos de cada uno
     public static void listarBugs() {
         StringBuilder lista = new StringBuilder("Lista de bugs: \n" + "\n");
-        for (int i = 0; i < cantidadBugs; i++) {
-            lista.append(bugs[i].GetDetalle()).append("\n");
+        if (cantidadBugs == 0) {
+            Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder listar luego.",
+                    "Error", 0);
+        } else {
+            for (int i = 0; i < cantidadBugs; i++) {
+                lista.append(bugs[i].GetDetalle()).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, lista);
         }
-        JOptionPane.showMessageDialog(null, lista);
     }
-    // buscar bugs por ID o por estado
+
+    // #region Métodos buscadores
+    // buscar bugs por ID
+    public static void buscarBugPorID() {
+        if (cantidadBugs == 0) {
+            Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
+                    "Error", 0);
+        } else {
+            int iDingresado = Herramientas.solicitarEntero("Ingresá el ID (Número entero)");
+            int inicio = 0;
+            int fin = cantidadBugs - 1; // Adjust to the number of registered bugs
+
+            while (inicio <= fin) {
+                int centro = (inicio + fin) / 2;
+
+                if (iDingresado < bugs[centro].getNumeroId()) {
+                    fin = centro - 1;
+                } else if (iDingresado > bugs[centro].getNumeroId()) {
+                    inicio = centro + 1;
+                } else {
+                    Herramientas.mostrarMensajes("Bug encontrado: \n" + "\n" + bugs[centro].GetDetalle(), "Búsqueda por ID", 1);
+                    return; // Exit the method once the bug is found
+                }
+            }
+            Herramientas.mostrarMensajes("Bug no encontrado", "Búsqueda por ID", 0);
+        }
+    }
+
+    // buscar bugs por título
+    public static void buscarBugPorTitulo() {
+        if (cantidadBugs == 0) {
+            Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
+                    "Error", 0);
+        } else {
+
+        }
+    }
+
+    // buscar bugs por estado
+    public static void buscarBugPorEstado() {
+        if (cantidadBugs == 0) {
+            Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
+                    "Error", 0);
+        } else {
+
+        }
+    }
+
+    // buscar bugs por severidad
+    public static void buscarBugPorSeveridad() {
+        if (cantidadBugs == 0) {
+            Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
+                    "Error", 0);
+        } else {
+
+        }
+    }
+    // #endregion
+
     // ordenar bugs por prioridad o fecha de creacion
     // modificar bug, titulo, descripcion, prioridad o estado, solicitar ID del bug
     // a modificar
--- BugTracker/src/view/Main.java
+++ BugTracker/src/view/Main.java
@@ -8,18 +8,72 @@ import model.Bug;
 // Clase Principal
 public class Main {
     public static void main(String[] args) {
-        // crear algunos bugs
-        BugTracker.crearBug();
-        // mostrar cantidad de bugs
-        JOptionPane.showMessageDialog(null, "Cantidad de bugs: " + Bug.getCantidadBug());
-        // mostrar datos de los bugs
-        /*
-         * for (int i = 0; i < BugTracker.cantidadBugs; i++) {
-         * JOptionPane.showMessageDialog(null, BugTracker.bugs[i].GetDetalle());
-         * }
-         */
-        // listar bugs
-        BugTracker.listarBugs();
 
+        boolean continuar = true;
+
+        while (continuar) {
+            String[] opciones = { "Crear Bug", "Lista de Bugs", "Modificar Bug", "Eliminar Bug", "Buscar Bug",
+                    "Informes",
+                    "Salir" };
+            int selection = JOptionPane.showOptionDialog(null, "¿Qué desea hacer?",
+                    "BugTracker", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones,
+                    opciones[0]);
+
+            switch (selection) {
+                case 0:
+                    // crear algunos bugs
+                    BugTracker.crearBug();
+                    break;
+                case 1:
+                    BugTracker.listarBugs();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    boolean seguir = true;
+                    while (seguir) {
+                        String[] opcionesBusqueda = { "Búsqueda por ID", "Búsqueda por título", "Búsqueda por estado",
+                                "Búsqueda por severidad",
+                                "Salir" };
+                        int selectionBusqueda = JOptionPane.showOptionDialog(null, "Qué tipo de búsqueda desea usar?",
+                                "BugTracker", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+                                opcionesBusqueda,
+                                opcionesBusqueda[0]);
+
+                        switch (selectionBusqueda) {
+                            case 0:
+                                BugTracker.buscarBugPorID();
+                                break;
+                            case 1:
+                                BugTracker.buscarBugPorTitulo();
+                                break;
+                            case 2:
+                                BugTracker.buscarBugPorEstado();
+                                break;
+                            case 3:
+                                BugTracker.buscarBugPorSeveridad();
+                                break;
+                            case 4:
+                                seguir = false;
+                                break;
+                            default:
+                                seguir = false;
+                                break;
+                        }
+                    }
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa");
+                    continuar = false;
+                    break;
+                default:
+                    continuar = false;
+                    break;
+            }
+        }
     }
 }
