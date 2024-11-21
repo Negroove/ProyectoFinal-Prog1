0b68838f04057bbab0934bbb8795c325ef3ca2c5
David Niz <dalos58.arg@gmail.com>
2024-11-21 12:01:20 -0300

    Agregada función listarBugs

--- BugTracker/src/service/BugTracker.java
+++ BugTracker/src/service/BugTracker.java
@@ -1,13 +1,16 @@
 package service;
 
+import javax.swing.JOptionPane;
+
 import model.Bug;
 
 // Logica / Reglas de Negocio
-// permitir al usuario crear un bug
+
 public class BugTracker {
     public static Bug[] bugs = new Bug[100];
     public static int cantidadBugs = 0;
 
+    // crear bug
     public static Bug crearBug() {
 
         Bug bug = new Bug();
@@ -19,4 +22,19 @@ public class BugTracker {
 
     }
 
+    // listar bugs mostrando una lista general con los datos de cada uno
+    public static void listarBugs() {
+        StringBuilder lista = new StringBuilder("Lista de bugs: \n" + "\n");
+        for (int i = 0; i < cantidadBugs; i++) {
+            lista.append(bugs[i].GetDetalle()).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, lista);
+    }
+    // buscar bugs por ID o por estado
+    // ordenar bugs por prioridad o fecha de creacion
+    // modificar bug, titulo, descripcion, prioridad o estado, solicitar ID del bug
+    // a modificar
+    // eliminar bug, solicitar ID del bug a eliminar
+    // generar informe de bugs, mostrar cantidad de bugs, cantidad de bugs por
+    // estado, cantidad de bugs por prioridad
 }
