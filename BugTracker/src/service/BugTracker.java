6fe65b56e353d4032fa3a6ada239a71c2b58c3f2
David Niz <dalos58.arg@gmail.com>
2024-11-22 16:31:26 -0300

    Busquedas por título, estado y severidad completas.

--- BugTracker/src/service/BugTracker.java
+++ BugTracker/src/service/BugTracker.java
@@ -3,6 +3,8 @@ package service;
 import javax.swing.JOptionPane;
 
 import model.Bug;
+import model.Bug.Severidad;
+import model.Incidence.Estado;
 import utils.Herramientas;
 
 // Logica / Reglas de Negocio
@@ -56,7 +58,8 @@ public class BugTracker {
                 } else if (iDingresado > bugs[centro].getNumeroId()) {
                     inicio = centro + 1;
                 } else {
-                    Herramientas.mostrarMensajes("Bug encontrado: \n" + "\n" + bugs[centro].GetDetalle(), "Búsqueda por ID", 1);
+                    Herramientas.mostrarMensajes("Bug encontrado: \n" + "\n" + bugs[centro].GetDetalle(),
+                            "Búsqueda por ID", 1);
                     return; // Exit the method once the bug is found
                 }
             }
@@ -66,31 +69,80 @@ public class BugTracker {
 
     // buscar bugs por título
     public static void buscarBugPorTitulo() {
+        boolean encontrado = false;
         if (cantidadBugs == 0) {
             Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
                     "Error", 0);
         } else {
-
+            String tituloBuscado = Herramientas.solicitarTexto("Ingresá el título del bug que buscas");
+            StringBuilder titulosEncontrados = new StringBuilder("Coincidencias encontradas:\n" + "\n");
+            for (int i = 0; i < cantidadBugs; i++) {
+                if (bugs[i].getTitulo().contains(tituloBuscado.toLowerCase())) {
+                    titulosEncontrados.append(bugs[i].GetDetalle()).append("\n");
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                Herramientas.mostrarMensajes(titulosEncontrados.toString(), "Búsqueda por título", 1);
+            } else {
+                Herramientas.mostrarMensajes(
+                        "No se ha encontrado ninguna coincidencia de título con la palabra: " + tituloBuscado,
+                        "Búsqueda por título", 1);
+            }
         }
     }
 
     // buscar bugs por estado
     public static void buscarBugPorEstado() {
+        boolean encontrado = false;
         if (cantidadBugs == 0) {
             Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
                     "Error", 0);
         } else {
-
+            StringBuilder estadosEncontrados = new StringBuilder("Coincidencias encontradas:\n" + "\n");
+            Estado estadoBuscado = Herramientas.mostrarOpcionesConEnum(
+                    "Seleccione el estado con el que desea filtrar: ",
+                    Bug.Estado.class);
+            for (int i = 0; i < cantidadBugs; i++) {
+                if (bugs[i].getEstado() == estadoBuscado) {
+                    estadosEncontrados.append(bugs[i].GetDetalle()).append("\n");
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                Herramientas.mostrarMensajes(estadosEncontrados.toString(), "Búsqueda por estado", 1);
+            } else {
+                Herramientas.mostrarMensajes(
+                        "No hay bugs registrados con estado " + estadoBuscado,
+                        "Búsqueda por estado", 0);
+            }
         }
     }
 
     // buscar bugs por severidad
     public static void buscarBugPorSeveridad() {
+        boolean encontrado = false;
         if (cantidadBugs == 0) {
             Herramientas.mostrarMensajes("No hay bugs registrados, asegurate de registrar para poder buscar luego.",
                     "Error", 0);
         } else {
-
+            StringBuilder severidadEncontrados = new StringBuilder("Coincidencias encontradas:\n" + "\n");
+            Severidad severidadBuscada = Herramientas.mostrarOpcionesConEnum(
+                    "Seleccione la severidad con la que desea filtrar: ",
+                    Bug.Severidad.class);
+            for (int i = 0; i < cantidadBugs; i++) {
+                if (bugs[i].getSeveridad() == severidadBuscada) {
+                    severidadEncontrados.append(bugs[i].GetDetalle()).append("\n");
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                Herramientas.mostrarMensajes(severidadEncontrados.toString(), "Búsqueda por severidad", 1);
+            } else {
+                Herramientas.mostrarMensajes(
+                        "No hay bugs registrados con severidad " + severidadBuscada,
+                        "Búsqueda por estado", 0);
+            }
         }
     }
     // #endregion
